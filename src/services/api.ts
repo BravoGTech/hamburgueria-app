f64c162266d5bf8b55a1bce21388ce1ee266b302
Igor <igorgg@gmail.com>
2023-05-10 22:46:09 -0300

    changing to aws api

--- src/services/api.ts
+++ src/services/api.ts
@@ -2,5 +2,6 @@ import axios from "axios";
 
 export const api = axios.create({
   // baseURL: "http://127.0.0.1:3000",
-  baseURL: "https://downtown-api.onrender.com",
+  // baseURL: "https://downtown-api.onrender.com",
+  baseURL: "ec2-54-94-94-133.sa-east-1.compute.amazonaws.com:3000",
 });
