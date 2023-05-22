acbf57589c03594a14149979fc1029c0dff3c366
Igor <igorgg@gmail.com>
2023-05-22 14:12:39 -0300

    deploy: change baseURL to aws

--- src/services/api.ts
+++ src/services/api.ts
@@ -2,6 +2,6 @@ import axios from "axios";
 
 export const api = axios.create({
   // baseURL: "http://127.0.0.1:3000",
-  baseURL: "https://downtown-api.onrender.com",
-  // baseURL: "https://bgm1qceg49.execute-api.sa-east-1.amazonaws.com/DonwTown/",
+  // baseURL: "https://downtown-api.onrender.com",
+  baseURL: "http://ec2-18-231-111-149.sa-east-1.compute.amazonaws.com:3000",
 });
