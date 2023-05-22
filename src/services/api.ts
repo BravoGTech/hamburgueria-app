7fd2ea650716d1b68df02f074aa02b39c72a6037
Igor Garcia <igorgg@gmail.com>
2023-05-22 14:21:33 -0300

    Update api.ts

--- src/services/api.ts
+++ src/services/api.ts
@@ -3,5 +3,5 @@ import axios from "axios";
 export const api = axios.create({
   // baseURL: "http://127.0.0.1:3000",
   // baseURL: "https://downtown-api.onrender.com",
-  baseURL: "http://ec2-18-231-111-149.sa-east-1.compute.amazonaws.com:3000",
+  baseURL: "https://ec2-18-231-111-149.sa-east-1.compute.amazonaws.com:3000",
 });
